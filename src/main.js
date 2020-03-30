560ec4b7979e72d30c0eea7e5ff4d52f31c50604
Michael Alt <mail@michael-alt.info>
2020-03-30 23:24:54 +0200

    Implement the app with react elements

--- src/main.js
+++ src/main.js
@@ -1,13 +1,56 @@
 // Main Entry Point
 
-import {getDefaultCurrency} from './util/default-currency.js';
+import {getDefaultCurrency, setDefaultCurrency} from './util/default-currency.js';
+import {getRates} from './util/rates-api.js';
 
-const baseCurrency = getDefaultCurrency();
+function renderUI() {
 
-const ratesHeader = React.createElement(
-  'h2',
-  { className: 'text-3xl pb-2 border-b border-gray-800'},
-  'Aktuelle Kurse des ' + baseCurrency
-);
+  const baseCurrency = getDefaultCurrency();
+  getRates(baseCurrency).then( data => {
 
-ReactDOM.render(ratesHeader, document.getElementById('ratesHeader'));
+    const date = new Date(data.date).toLocaleDateString('de-DE');
+    const ratesHeader = React.createElement(
+      'h2',
+      { className: 'text-3xl pb-2 border-b border-gray-800'},
+      'Aktuelle Kurse des ' + baseCurrency,
+      React.createElement('small', { className: 'ml-4' }, `(Stand vom ${date})`)
+    );
+
+    const currencies = Object.keys(data.rates).sort();
+    const currencyList = React.createElement(
+      'ul',
+      { className: 'w-24 text-blue-700 text-center mx-auto border-b border-blue-200'},
+      currencies.map(c => {
+        let classNames = 'w-24 py-2 border border-blue-200 border-b-0';
+        if (c === baseCurrency) {
+          classNames += ' bg-blue-500 text-white';
+        } else {
+          classNames += ' bg-white hover:bg-gray-100';
+        }
+        return React.createElement('li', { key: c },
+          React.createElement('button', { className: classNames }, c));
+      })
+    );
+
+    const rates = React.createElement( React.Fragment, null,
+      currencies.map(c => React.createElement('tr', { key: c },
+        React.createElement('td', null, c),
+        React.createElement('td', { className: 'text-right' }, data.rates[c].toFixed(4) ),
+      ))
+    );
+
+    ReactDOM.render(ratesHeader, document.getElementById('ratesHeader'));
+    ReactDOM.render(currencyList, document.getElementById('currencyList'));
+    ReactDOM.render(rates, document.getElementById('rates'));
+  });
+
+}
+
+renderUI();
+
+document.getElementById('currencyList').addEventListener('click', ev => {
+  if (ev.target.matches('button')) {
+    setDefaultCurrency(ev.target.textContent);
+    renderUI();
+  }
+});
