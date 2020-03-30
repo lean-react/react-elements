14cdc8e2d136135dc21e156130e13e40dd912f53
Michael Alt <mail@michael-alt.info>
2020-03-30 22:34:23 +0200

    Use react the first time.

--- src/main.js
+++ src/main.js
@@ -1,1 +1,13 @@
 // Main Entry Point
+
+import {getDefaultCurrency} from './util/default-currency.js';
+
+const baseCurrency = getDefaultCurrency();
+
+const ratesHeader = React.createElement(
+  'h2',
+  { className: 'text-3xl pb-2 border-b border-gray-800'},
+  'Aktuelle Kurse des ' + baseCurrency
+);
+
+ReactDOM.render(ratesHeader, document.getElementById('ratesHeader'));
