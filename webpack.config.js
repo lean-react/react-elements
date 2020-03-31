e8c6e6ddc37e161424e14fc3010e3c8f689523f1
Michael Alt <mail@michael-alt.info>
2020-03-31 22:42:15 +0200

    Optimize webpack builds.

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,41 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 
-module.exports = {
-  mode: "development",
+module.exports = (env, argv) => {
 
-  entry: './src/main.js',
-  output: {
-    filename: "main.bundle.js"
-  },
+  const isProduction = argv.mode === 'production';
 
-  devtool: 'inline-source-map',
-  devServer: {
-    contentBase: './dist'
-  },
+  return {
+    mode: 'development',
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: './src/index.html'
-    })
-  ]
+    entry: './src/main.js',
+    output: {
+      filename: "[name].[contenthash].js"
+    },
+    optimization: {
+      moduleIds: 'hashed',
+      runtimeChunk: 'single',
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: 'vendors',
+            chunks: 'all'
+          }
+        }
+      }
+    },
+
+    devtool: isProduction ? 'source-map' : 'inline-source-map',
+    devServer: {
+      contentBase: './dist'
+    },
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html'
+      })
+    ]
+  }
 };
